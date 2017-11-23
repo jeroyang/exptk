761864af1d980eb9febf577c1bf7f6905f76e9df
jeroyang <jeroyang@gmail.com>
2017-11-23 16:17:53 +0800

    fix

--- setup.py
+++ setup.py
@@ -39,6 +39,5 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
-    test_suite='tests',
-    tests_require=test_requirements
+    test_suite='tests'
 )
