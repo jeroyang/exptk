7fe9b7107e306bc056fbcb5890815e0a88956c7e
jeroyang <jeroyang@gmail.com>
2017-11-26 17:50:32 +0800

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name='exptk',
-    version='0.1.0',
+    version='0.2.0',
     description="A toolkit for building a machine learning project",
     long_description=readme,
     author="Chia-Jung, Yang",
